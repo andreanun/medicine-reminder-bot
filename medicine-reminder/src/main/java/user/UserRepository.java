335a509e6453d1bd73a234a5592d98a9e9543a45
Andrea Nunoo <123588571+andreanun@users.noreply.github.com>
2026-01-22 20:42:14 -0500

    added user entity and repository interface

--- medicine-reminder/src/main/java/user/UserRepository.java
+++ medicine-reminder/src/main/java/user/UserRepository.java
@@ -1,4 +1,13 @@
 package user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByPhoneNumber(String phoneNumber);
+    boolean existsByPhoneNumber(String phoneNumber);
 }
